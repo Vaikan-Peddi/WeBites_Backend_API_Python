171dd7ecbe86feab41200340de5617be40e5d840
Vaikan Peddi <vaikanpeddi14@gmail.com>
2024-06-12 06:14:13 +0530

    minor changes

---
+++ DEVDOCS.md
@@ -0,0 +1,1 @@
+do the database migrations for now using SQL scripts and python drivers for sqlite. then later, work on alembic kinda tools
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
 
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
@@ -10,3 +11,14 @@ class User(Base):
     email = Column(String, unique=True)
     hashed_pwd = Column(String)
 
+    college_id = Column(ForeignKey("colleges.id"))
+
+
+    college = relationship("colleges")
+
+class College(Base):
+    __tablename__ = "colleges"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    state = Column(String, index=True)
